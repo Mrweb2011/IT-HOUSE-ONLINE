20c5ae6cdf4c4fceac19da357c1b5c080c8e97ab
Mukhammadrasul2011 <msamaritdinov@gmail.com>
2024-11-25 09:31:34 +0500

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,16 @@ import { LuMoon } from "react-icons/lu";
 function App() {
   return (
   <>
-    <header className='flex justify-center py-2 bg-white z-10 fixed mx-72'>
-      <div className="flex justify-between gap-72">
+    <header className='flex py-3 bg-white z-10 fixed px-16'>
+      <div className="flex justify-between gap-96">
       <div className='flex items-center gap-5'>
         <div className="flex gap-2 items-center">
           <button className='flex items-center gap-2'>
-          <img className='w-9 mt-2' src={Logo} alt="" />
-          <h1 className='font-semibold text-2xl'>IT HOUSE</h1>
+          <img className='w-11 mt-2' src={Logo} alt="" />
+          <h1 className='font-bold text-3xl'>IT HOUSE</h1>
           </button>
         </div>
-      <ul className='flex gap-3 items-center font-semibold text-xs'>
+      <ul className='flex gap-3 items-center font-bold'>
         <li className='text-blue-600'>Bosh sahifa</li>
         <li className='hover:text-blue-600 transition'>Kurslar</li>
         <li className='hover:text-blue-600 transition'>Treyding</li>
@@ -27,12 +27,12 @@ function App() {
       </ul>
       </div>
       <div className="flex gap-2 items-center">
-        <button className='bg-none border-none text-xl p-1 hover:bg-gray-100 rounded'><IoSearchOutline /></button>
-        <button className='bg-none border-none text-xl p-1 hover:bg-gray-100 rounded'><LuLanguages /></button>
-        <button className='bg-none border-none text-xl p-1 hover:bg-gray-100 rounded'><FiBell /></button>
-        <button className='bg-none border-none text-xl p-1 hover:bg-gray-100 rounded'><SlBasket /></button>
-        <button className='bg-none border-none text-xl p-1 hover:bg-gray-100 rounded'><LuMoon /></button>
-        <button className='bg-red-500 text-white py-3 px-7 text-xs rounded-3xl'>Kirish</button>
+        <button className='bg-none border-none text-2xl p-2 hover:bg-gray-100 rounded-lg'><IoSearchOutline /></button>
+        <button className='bg-none border-none text-2xl p-2 hover:bg-gray-100 rounded-lg'><LuLanguages /></button>
+        <button className='bg-none border-none text-2xl p-2 hover:bg-gray-100 rounded-lg'><FiBell /></button>
+        <button className='bg-none border-none text-2xl p-2 hover:bg-gray-100 rounded-lg'><SlBasket /></button>
+        <button className='bg-none border-none text-2xl p-2 hover:bg-gray-100 rounded-lg'><LuMoon /></button>
+        <button className='bg-red-500 text-white py-3 px-9 rounded-3xl'>Kirish</button>
       </div>
       </div>
     </header>
@@ -40,11 +40,11 @@ function App() {
     <div className="flex justify-center mt-40">
       <div className="flex justify-between gap-10">
         <div className="column">
-          <h1 className='text-4xl font-bold'>Kelajak kasblarini <br />o`rganishni hoziroq <span className='text-blue-600'>biz <br />bilan boshlang.</span></h1>
-          <h4 className='text-gray-400 mt-3 text-xs'>IT sohasidagi barcha sifatli kurslar. Biz bilan IT sohasini o'rganish endi <br />yanada osonroq shunchaki boshlang.</h4>
-          <button className='py-2 font-semibold mt-5 text-xs px-5 border-2 rounded-3xl hover:bg-gray-100 transition'>Kurslarni toping</button>
+          <h1 className='text-5xl font-bold'>Kelajak kasblarini <br />o`rganishni hoziroq <span className='text-blue-600'>biz <br />bilan boshlang.</span></h1>
+          <h4 className='text-gray-400 mt-3'>IT sohasidagi barcha sifatli kurslar. Biz bilan IT sohasini o'rganish endi <br />yanada osonroq shunchaki boshlang.</h4>
+          <button className='py-3 font-semibold mt-5 px-9 border-2 rounded-full hover:bg-gray-100 transition'>Kurslarni toping</button>
         </div>
-        <img className='w-96' src={BigImg} alt="" />
+        <img className='' src={BigImg} alt="" />
       </div>
     </div>
   </>
